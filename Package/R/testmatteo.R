a64388cce17ff9ff5d1766fc5036a3876766dce1
Mattéo <matteo.s@outlook.fr>
2025-03-08 19:57:56 +0100

    ajout docu

--- Package/R/testmatteo.R
+++ Package/R/testmatteo.R
@@ -32,24 +32,7 @@ server <- function(input, output, session) {
   nCols <- 6
   niveau = "moyen"
 
-  rv <- reactiveValues(grille = generer_takuzu(niveau, nRows))
-
-  # Générer la grille initiale
-  rv <- reactiveValues(
-    grille = generer_takuzu(niveau, nRows),  # Matrice de la grille
-    disabled = matrix(FALSE, nrow = nRows, ncol = nCols)  # Matrice pour désactiver les boutons
-  )
-
-  # Désactiver les boutons préremplis initialement
-
-  observe({
-    cases_preremplies <- which(!is.na(rv$grille), arr.ind = TRUE)
-    for (k in 1:nrow(cases_preremplies)) {
-      i <- cases_preremplies[k, 1]
-      j <- cases_preremplies[k, 2]
-      rv$disabled[i, j] <- TRUE  # Désactiver les boutons préremplis
-    }
-  })
+  rv <- reactiveValues(grille = generer_takuzu(niveau, nRows))  # Initialisation de la grille avec NA
 
   output$grille_boutons <- renderUI({
     boutons <- lapply(1:nRows, function(i) {
@@ -57,8 +40,7 @@ server <- function(input, output, session) {
         lapply(1:nCols, function(j) {
           actionButton(inputId = paste("bouton", i, j, sep = "_"),
                        label = ifelse(is.na(rv$grille[i, j]), "", as.character(rv$grille[i, j])),
-                       style = "width: 100px; height: 100px; font-size: 18px; margin: 5px;",
-                       disabled = rv$disabled[i, j])  # Désactive le bouton si la case est préremplie initialement
+                       style = "width: 100px; height: 100px; font-size: 18px; margin: 5px;")
         })
       )
     })
@@ -71,13 +53,7 @@ server <- function(input, output, session) {
       observeEvent(input[[paste("bouton", i, j, sep = "_")]], {
         # Mettre à jour la valeur de la case
         valeur_actuelle <- rv$grille[i, j]
-        if (is.na(valeur_actuelle)) {
-          valeur_nouvelle <- 0  # Si la case est vide, on la remplit avec 0
-        } else if (valeur_actuelle == 0) {
-          valeur_nouvelle <- 1  # Si la case est 0, on la change en 1
-        } else {
-          valeur_nouvelle <- NA  # Si la case est 1, on la change en NA
-        }
+        valeur_nouvelle <- ifelse(is.na(valeur_actuelle), 0, ifelse(valeur_actuelle == 0, 1, NA))
         rv$grille[i, j] <- valeur_nouvelle
 
         # Mettre à jour l'étiquette du bouton
@@ -97,10 +73,3 @@ server <- function(input, output, session) {
 
 # Lancer l'application
 shinyApp(ui = ui, server = server)
-
-
-
-grille = generer_takuzu("moyen", 8)
-rv = reactiveValues(grille = generer_takuzu("moyen", 8))
-cases_premplies <- which(!is.na(rv$grille), arr.ind = TRUE)
-
