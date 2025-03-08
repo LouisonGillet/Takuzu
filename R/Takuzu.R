057faeec349f57bb3091a5bc34a8be7e23017d21
Mattéo <matteo.s@outlook.fr>
2025-03-08 12:44:07 +0100

    ajout de grille et des règles

--- R/Takuzu.R
+++ R/Takuzu.R
@@ -1,35 +1,60 @@
 library(shiny)
 
+
+
+
+
 ui <- fluidPage(
-  titlePanel("Jeu Takuzu 5x5"),
-  fluidRow(
-    lapply(1:5, function(i) {
-      column(2,
-             lapply(1:5, function(j) {
-               actionButton(inputId = paste("cell_", i, j, sep = "_"),
-                            label = "",
-                            width = "50px",
-                            style = "height: 50px; font-size: 18px")
-             })
-      )
-    })
-  ),
-  actionButton("Commencer", "Commencer une nouvelle partie"),
-  actionButton("Verification", "Vérifier la solution"),
-  textOutput("status")
+  titlePanel("Jeu Takuzu"),
+  h3("Remplissez la grille avec des 0 et 1"),
+  uiOutput("grille_boutons")
 )
 
 
-server <- function(input, output, session) {
 
-  grid <- reactiveVal(matrix(NA, nrow = 5, ncol = 5))  # Une matrice 5x5 vide
-  observeEvent(input$Commencer, {
-    grid(matrix(NA, nrow = 5, ncol = 5))  # Réinitialisation de la matrice
+
+server <- function(input, output, session) {
+  nRows <- 5
+  nCols <- 5
+
+  rv <- reactiveValues(grille = matrix(NA, nrow = nRows, ncol = nCols))  # Initialisation de la grille avec NA
+
+  output$grille_boutons <- renderUI({
+    boutons <- lapply(1:nRows, function(i) {
+      fluidRow(
+        lapply(1:nCols, function(j) {
+          actionButton(inputId = paste("bouton", i, j, sep = "_"),
+                       label = ifelse(is.na(rv$grille[i, j]), "", as.character(rv$grille[i, j])),
+                       style = "width: 100px; height: 100px; font-size: 18px; margin: 5px;")
+        })
+      )
+    })
+    tagList(boutons)
   })
 
-}
+  # Observer les clics sur les boutons pour les mettre à jour
+  lapply(1:nRows, function(i) {
+    lapply(1:nCols, function(j) {
+      observeEvent(input[[paste("bouton", i, j, sep = "_")]], {
+        # Mettre à jour la valeur de la case
+        valeur_actuelle <- rv$grille[i, j]
+        valeur_nouvelle <- ifelse(is.na(valeur_actuelle), 0, ifelse(valeur_actuelle == 0, 1, NA))
+        rv$grille[i, j] <- valeur_nouvelle
 
+        # Mettre à jour l'étiquette du bouton
+        updateActionButton(
+          session,
+          paste("bouton", i, j, sep = "_"),
+          label = ifelse(is.na(valeur_nouvelle), "", as.character(valeur_nouvelle))
+        )
+
+        # Affichage des messages de débogage
+        print(paste("Avant changement: Case (", i, ",", j, ") -> ", valeur_actuelle))
+        print(paste("Après changement: Case (", i, ",", j, ") -> ", valeur_nouvelle))
+      })
+    })
+  })
+}
 
 # Lancer l'application
 shinyApp(ui = ui, server = server)
-
