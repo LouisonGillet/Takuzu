835c82637e423453abb4c8ec2beb65608067f96a
Mattéo <matteo.s@outlook.fr>
2025-04-18 10:04:12 +0200

    fix

--- Scaiet/R/run_app.R
+++ Scaiet/R/run_app.R
@@ -2,8 +2,7 @@
 #'
 #' @description
 #' Cette fonction permet de lancer l'application Shiny embarquée dans le package.
-#' Elle recherche automatiquement le répertoire contenant l'application
-#' et l'exécute via `shiny::runApp()`. Elle est destinée à être utilisée par l'utilisateur
+#' L'exécute via `shiny::runApp()`. Elle est destinée à être utilisée par l'utilisateur
 #' final du package.
 #'
 #' @return Aucune valeur de retour. Cette fonction a pour effet de démarrer une session Shiny.
@@ -18,9 +17,10 @@
 run_app <- function() {
   appDir <- "./Scaiet/app/app.R"
   
-  if (!dir.exists(appDir)) {
-    stop("Le dossier de l'application est introuvable : ", appDir)
+  if (!file.exists(appDir)) {
+    stop("Le fichier de l'application est introuvable : ", appDir)
   }
   
-  shiny::runApp(appDir, display.mode = "normal")
+  shiny::runApp(dirname(appDir), display.mode = "normal")
 }
+
